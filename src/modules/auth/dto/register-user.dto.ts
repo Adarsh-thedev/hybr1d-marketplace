58ae6337d7baed9f87d9feaa5357f5817374e398
Adarsh Singh <adarshpittu@gmail.com>
2023-01-22 12:50:56 +0530

    register user dto swagger

--- src/modules/auth/dto/register-user.dto.ts
+++ src/modules/auth/dto/register-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsEnum,
   IsNotEmpty,
@@ -8,16 +9,19 @@ import {
 import { UserTypes } from 'src/common/enums';
 
 export class RegisterUserDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @MinLength(3)
   userName: string;
 
+  @ApiProperty()
   @Matches(/^[ A-Za-z0-9_@./#&+-]*$/)
   @MinLength(6)
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   @IsEnum(UserTypes)
