50d5d42d6bbc47dd5586de54070fb6a75c5dd05f
Adarsh Singh <adarshpittu@gmail.com>
2023-01-22 12:51:50 +0530

    create-order dto swagger

--- src/modules/buyers/dto/create-order.dto.ts
+++ src/modules/buyers/dto/create-order.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { ArrayNotEmpty, IsArray } from 'class-validator';
 
 export class CreateOrderDto {
+  @ApiProperty()
   @IsArray()
   @ArrayNotEmpty()
   products: Array<string>;
