daaa1f57ac71485f5140645687082f1e8ad18a37
Adarsh Singh <adarshpittu@gmail.com>
2023-01-22 12:49:25 +0530

    swagger documentaion added

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { CONSTANTS } from './utils/constants';
 
@@ -22,6 +23,15 @@ async function bootstrap() {
   app.setGlobalPrefix(CONSTANTS.GLOBAL_PREFIX, {
     exclude: [{ path: '', method: RequestMethod.GET }],
   });
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle(CONSTANTS.APPLICATION_NAME)
+    .setDescription(`${CONSTANTS.APPLICATION_NAME} APIs`)
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(configService.get(CONSTANTS.PORT) ?? 8000);
   app.enableCors();
