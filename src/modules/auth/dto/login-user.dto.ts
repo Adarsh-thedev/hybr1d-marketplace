54f61272c336971e5d03cc9749a69d895f88769c
Adarsh Singh <adarshpittu@gmail.com>
2023-01-22 12:50:24 +0530

    login dto swagger

--- src/modules/auth/dto/login-user.dto.ts
+++ src/modules/auth/dto/login-user.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LoginUserDto {
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   userName: string;
 
+  @ApiProperty()
   @IsString()
   @IsNotEmpty()
   password: string;
